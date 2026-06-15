e61ab4b9abce133571ebbe6cb829b5992ef6db49
ElDauda <abuabdullahi032020@gmail.com>
2026-06-15 14:08:44 -0700

    Fix Render port binding

--- run_dashboard.py
+++ run_dashboard.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -7,4 +8,16 @@ sys.path.append(str(project_root))
 from src.dashboard.dashboard import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            8050
+        )
+    )
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False,
+    )
